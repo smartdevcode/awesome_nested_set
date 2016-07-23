9400b2144c55cd77e697b017ce26a66d4f3d0b89
SeniorDevCode <senior.dev.software@gmail.com>
2016-07-23 16:09:21 +0200

    use rspec-rails ~> 3.5.0

--- lib/awesome_nested_set.rb
+++ lib/awesome_nested_set.rb
@@ -1,11 +1,8 @@
-require 'active_support/lazy_load_hooks'
 require 'awesome_nested_set/awesome_nested_set'
+require 'active_record'
+ActiveRecord::Base.send :extend, CollectiveIdea::Acts::NestedSet
 
-ActiveSupport.on_load(:active_record) do
-  ActiveRecord::Base.send :extend, CollectiveIdea::Acts::NestedSet
-end
-
-ActiveSupport.on_load(:action_view) do
+if defined?(ActionView)
   require 'awesome_nested_set/helper'
   ActionView::Base.send :include, CollectiveIdea::Acts::NestedSet::Helper
 end
