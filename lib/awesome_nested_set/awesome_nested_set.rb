131633f157ad7770da830461cf60fa10af42909d
SeniorDevCode <senior.dev.software@gmail.com>
2016-10-02 15:44:14 +0200

    Specify that attr_accessible is for Rails < 4

--- lib/awesome_nested_set/awesome_nested_set.rb
+++ lib/awesome_nested_set/awesome_nested_set.rb
@@ -98,8 +98,7 @@ module CollectiveIdea #:nodoc:
                             :counter_cache => acts_as_nested_set_options[:counter_cache],
                             :inverse_of => (:children unless acts_as_nested_set_options[:polymorphic]),
                             :polymorphic => acts_as_nested_set_options[:polymorphic],
-                            :touch => acts_as_nested_set_options[:touch],
-                            :optional => true
+                            :touch => acts_as_nested_set_options[:touch]
       end
 
       def acts_as_nested_set_default_options
